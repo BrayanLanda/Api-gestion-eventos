b6e4c25b3f1a8da5c4fcf4d088c58bcf96216f6c
DesarrolloFunerariaSanVicente <desarrollo@funerariasanvicente.com>
2026-08-11 17:55:09 -0500

    first commit

---
+++ src/main/java/org/gestion/eventos/api/ApiGestionApplication.java
@@ -0,0 +1,13 @@
+package org.gestion.eventos.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGestionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGestionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/gestion/eventos/api/ApiGestionApplicationTests.java
@@ -0,0 +1,13 @@
+package org.gestion.eventos.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGestionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
